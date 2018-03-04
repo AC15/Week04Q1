0f29365d2176d3456b899e12115cad58ecb46474
AC15 <aleksander.czarnowski@gmail.com>
2018-03-04 21:50:01 +0000

    Added an add option to choice menu loop.

--- src/stack/StackApplication.java
+++ src/stack/StackApplication.java
@@ -6,25 +6,37 @@ import java.util.Scanner;
  * Created by AC15 on 18/02/2018
  */
 public class StackApplication {
+    private static ArrayStack<String> arrayStack = new ArrayStack<>();
+    private static LinkedStack<String> linkedStack = new LinkedStack<>();
+
     public static void main(String[] args) {
-        Stack<String> arrayStack = new ArrayStack<>();
-        Stack<String> linkedStack = new LinkedStack<>();
         Scanner scanner = new Scanner(System.in);
         boolean quit = true;
 
-        System.out.println("Choose an option.\n" +
-                "0: Quit");
-
         while (quit) {
+            System.out.println("Choose an option.\n" +
+                    "0: Quit\n" +
+                    "1: Add");
+
             int choice = scanner.nextInt();
 
             switch (choice) {
                 case 0:
                     quit = false;
                     break;
+                case 1:
+                    System.out.print("Input string: ");
+                    String input = scanner.next();
+                    add(input);
+                    break;
             }
         }
 
         scanner.close();
+    }
+
+    private static void add(String input) {
+        arrayStack.push(input);
+        linkedStack.push(input);
     }
 }
