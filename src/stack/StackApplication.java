8d5b7adfc70844b87b9aa034934979b1194d8866
AC15 <aleksander.czarnowski@gmail.com>
2018-03-04 21:10:19 +0000

    Added a choice menu loop.

--- src/stack/StackApplication.java
+++ src/stack/StackApplication.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Scanner;
+
 /**
  * Created by AC15 on 18/02/2018
  */
@@ -7,5 +9,22 @@ public class StackApplication {
     public static void main(String[] args) {
         Stack<String> arrayStack = new ArrayStack<>();
         Stack<String> linkedStack = new LinkedStack<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = true;
+
+        System.out.println("Choose an option.\n" +
+                "0: Quit");
+
+        while (quit) {
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 0:
+                    quit = false;
+                    break;
+            }
+        }
+
+        scanner.close();
     }
 }
